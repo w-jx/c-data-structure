85e62c61072179524c4228f5102be4775f15a9aa
mf1923078 <742359579@qq.com>
2020-10-24 07:48:18 +0800

    添加希尔排序,文件夹分组,利于阅读

---
+++ 数据结构/排序/希尔排序.cpp
@@ -0,0 +1,53 @@
+#if 1
+#include <iostream>
+#include "../myhead.h"
+using namespace std;
+/*
+2020/10/24
+希尔排序.cpp
+希尔排序,和插入排序很像,但是比插入排序优越
+*/
+
+void  shell_sort(vector<int>& v)
+{
+	int len = v.size();
+	if (len <= 1) return;
+
+	int step = len / 2;
+	int j, flag;
+
+	while (step !=0) {
+
+
+		for (auto i = step; i < len; i++) {
+			flag = v[i];
+			j = i - step;
+
+
+			while (j >= 0 && v[j] > flag) {
+				v[j + step] = v[j];
+				j -= step;
+			}
+			v[j + step] = flag;
+			
+		}
+		step--;
+
+	}
+
+}
+void test01()
+{
+	vector<int> v{ 1,3,2,7,10,18,6 ,-9 };
+	printVector(v);
+	shell_sort(v);
+	std::cout << "after shell sort:\n";
+	printVector(v);
+}
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
+#endif
